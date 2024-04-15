915a4d2ae031f7425339b49b11e86522a65c2203
Assassinyogeshh <140268831+Assassinyogeshh@users.noreply.github.com>
2024-04-15 22:39:27 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -47,11 +47,11 @@ const App = () => {
 
   return (
     <>
-      <div className="w-full h-[100vh] flex flex-col justify-center items-center gap-y-4 bg-purple-700">
-        <h1 className="text-[2rem] font-[600]">Add Your Important Task Here</h1>
-        <div className="flex flex-col justify-evenly items-center border h-[70%] w-[50%]">
+      <div className=" w-full h-[100vh] flex flex-col justify-center items-center gap-y-4 bg-purple-700">
+        <h1 className=" max-sm:text-[22px] text-[2rem] font-[600]">Add Your Important Task Here</h1>
+        <div className="flex flex-col justify-evenly items-center border max-lg:border-none h-[70%] w-[50%]">
           <form
-            className="flex justify-between items-center w-[70%] gap-2 h-[12%] rounded-[2rem] bg-zinc-200 "
+            className="flex justify-between items-center w-[70%] max-lg:h-[8%] max-sm:h-[5%] gap-2 h-[12%] rounded-[2rem] bg-zinc-200 "
             onSubmit={handleSubmit}
           >
             <input
@@ -60,12 +60,12 @@ const App = () => {
               name="task"
               value={inputValue}
               onChange={(e) => setInputValue(e.target.value)}
-              className="rounded-[1rem] h-[2rem] pl-2 bg-transparent placeholder:text-black text-black outline-none w-[100%]"
+              className="rounded-[1rem] max-sm:text-[12px] h-[2rem] pl-2 bg-transparent placeholder:text-black text-black outline-none w-[100%]"
               required
             />
             <button
               type="submit"
-              className=" bg-orange-600 text-white font-[500] text-[18px] h-full w-[37%] rounded-[2rem]"
+              className=" bg-orange-600 max-sm:text-[12px] text-white font-[500] text-[18px] h-full w-[37%] rounded-[2rem]"
             >
               Add
             </button>
@@ -76,13 +76,13 @@ const App = () => {
   <ul className="w-[70%] h-[40vh] overflow-y-auto gap-y-5">
     {data.map((userData) => (
       <li
-        className="w-full mt-2  bg-amber-600 flex justify-evenly items-center rounded-[2rem] h-[4rem]"
+        className="w-full mt-2  bg-amber-600 flex justify-evenly items-center rounded-[2rem] max-sm:h-[3rem] h-[4rem]"
         key={userData.id}
       >
         <span className="cursor-pointer" onClick={() => handleEdit(userData.id, userData.task)}>
           <FontAwesomeIcon icon={faPenToSquare} />
         </span>
-        <p className={`${userData.completed ? "line-through" : ""} overflow-hidden w-[50%] flex justify-center items-center`}>
+        <p className={`${userData.completed ? "line-through" : ""} max-sm:text-[18px] max-lg:text-[25px] overflow-hidden w-[50%] flex justify-center items-center`}>
           {userData.task}
         </p>
         {userData.completed ? (
